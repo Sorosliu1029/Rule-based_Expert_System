5277ea6a0bd232374d173bc2aa0cc73520af4a3a
Sorosliu1029 <soros.liu1029@gmail.com>
2016-01-11 12:24:50 +0800

    modify picture handler

--- Picture_handler/cv_handler.py
+++ Picture_handler/cv_handler.py
@@ -15,13 +15,13 @@ import math
 __author__ = 'Soros Liu'
 
 
-def calculate_line_length(line):
-    return ((line[0][0] - line[1][0]) ** 2 + (line[0][1] - line[1][1]) ** 2) ** 0.5
+def calculate_line_length(l):
+    return ((l[0][0] - l[1][0]) ** 2 + (l[0][1] - l[1][1]) ** 2) ** 0.5
 
 
-def calculate_line_angle(line):
+def calculate_line_angle(l):
     try:
-        k = (line[0][1] - line[1][1]) / (line[0][0] - line[1][0])
+        k = (l[0][1] - l[1][1]) / (l[0][0] - l[1][0])
     except ZeroDivisionError:
         k = 100000
     finally:
@@ -32,11 +32,11 @@ class HandledLine:
     """
 
     """
-    def __init__(self, line):
-        self.point1 = line[0]
-        self.point2 = line[1]
-        self.length = calculate_line_length(line)
-        self.angle = calculate_line_angle(line)
+    def __init__(self, l):
+        self.point1 = l[0]
+        self.point2 = l[1]
+        self.length = calculate_line_length(l)
+        self.angle = calculate_line_angle(l)
 
     def __str__(self):
         return '(%.2f, %.2f)-->(%.2f, %.2f): Length: %.2f, Angle:%.2f' % \
@@ -69,6 +69,12 @@ def average_line(line1, line2):
     return average_point(line1[0], line2[0]), average_point(line1[1], line2[1])
 
 
+def int2float(lines):
+    result = []
+    for l in lines:
+        result.append(((tuple(map(float, l[0]))), tuple(map(float, l[1]))))
+    return result
+
 class PreHandler:
     """
 
@@ -79,9 +85,11 @@ class PreHandler:
         self.src = LoadImage(source, 0)
         self.dst = CreateImage(GetSize(self.src), 8, 1)
         self.rst = CreateImage(GetSize(self.src), 8, 3)
+        self.hide = CreateImage(GetSize(self.src), 8, 3)
         Canny(self.src, self.dst, 10, 50, 3)
-        self.cv_lines = HoughLines2(self.dst, CreateMemStorage(0),
-                                    CV_HOUGH_PROBABILISTIC, 1, CV_PI/180, 50, 50, 10)
+        self.cv_int_lines = HoughLines2(self.dst, CreateMemStorage(0),
+                                    CV_HOUGH_PROBABILISTIC, 1, CV_PI/180, hough_argv, 50, 10)
+        self.cv_lines = int2float(self.cv_int_lines)
         map(self.calculate_gradient, self.cv_lines)
         self.cv_line_num = len(self.cv_lines)
         self.merge_lines()
@@ -108,7 +116,10 @@ class PreHandler:
 
     def draw_lines(self, lines):
         for line in lines:
-            Line(self.rst, line.point1, line.point2, CV_RGB(255, 0, 0), 1, CV_AA, 0)
+            Line(self.rst, tuple(map(int, line.point1)), tuple(map(int, line.point2)), CV_RGB(255, 0, 0), 1, CV_AA, 0)
+
+    def draw_hide(self, line):
+        Line(self.hide, line[0], line[1], CV_RGB(0, 255, 0), 1, CV_AA, 0)
 
 
 def get_meeting_point(line1, line2):
@@ -125,20 +136,39 @@ def get_meeting_point(line1, line2):
 def is_same_side(line1, line2, meeting_point):
     middle_point1 = average_point(line1.point1, line1.point2)
     middle_point2 = average_point(line2.point1, line2.point2)
-    return meeting_point < middle_point1 and meeting_point < middle_point2
+    # print('mdp1: ' + str(middle_point1) + '\nmdp2: ' +str(middle_point2) + '\nmet: ' + str(meeting_point))
+    return (meeting_point < middle_point1 and meeting_point < middle_point2) or \
+        (meeting_point > middle_point1 and meeting_point > middle_point2)
+
+
+def get_vector(from_point ,to_point):
+    return (to_point[0] - from_point[0]), (to_point[1] - from_point[1])
+
+
+def get_inner_product(vector1, vector2):
+    return vector1[0] * vector2[0] + vector1[1] * vector2[1]
 
 
 def get_included_angle(line1, line2):
     meeting_point = get_meeting_point(line1, line2)
-    included_angle = max(line1.angle, line2.angle) - min(line1.angle, line2.angle)
+    # included_angle = max(line1.angle, line2.angle) - min(line1.angle, line2.angle)
     if not meeting_point:
         return None          # lines not met
-    if meeting_point == 'parallel':
+    elif meeting_point == 'parallel':
         return 'parallel'    # parallel lines have no included angle
-    if is_same_side(line1, line2, meeting_point):
-        return included_angle if included_angle < 90.0 else 180 - included_angle
+    # if is_same_side(line1, line2, meeting_point):
+    #     return included_angle if included_angle < 90.0 else 180 - included_angle
+    # else:
+    #     return included_angle if included_angle > 90.0 else 180 - included_angle
     else:
-        return included_angle if included_angle > 90.0 else 180 - included_angle
+        middle_point1 = average_point(line1.point1, line1.point2)
+        middle_point2 = average_point(line2.point1, line2.point2)
+        vector1 = get_vector(meeting_point, middle_point1)
+        vector2 = get_vector(meeting_point, middle_point2)
+        inner_product = get_inner_product(vector1, vector2)
+        cos_theta = inner_product / (math.sqrt(get_inner_product(vector1, vector1)) *
+                                     math.sqrt(get_inner_product(vector2, vector2)))
+        return math.degrees(math.acos(cos_theta))
 
 
 def is_parallel(line1, line2):
@@ -157,6 +187,7 @@ def is_acute(line1, line2):
 
 def is_obtuse(line1, line2):
     included_angle = get_included_angle(line1, line2)
+    # print('angle: ' + str(included_angle) +'\n' + str(line1) + '\n' + str(line2))
     return included_angle and included_angle > 90.0
 
 
@@ -219,18 +250,25 @@ class FactGenerator:
 
 if __name__ == '__main__':
     # test
-    handler = PreHandler('../test/test.png')
+    hough_argv = 50
+    handler = PreHandler('../test/test9.png')
+    print('----hough lines---- ' + str(len(handler.cv_int_lines)))
+    for line in handler.cv_int_lines:
+        handler.draw_hide(line)
+        print(line)
+    print('-----end hough lines----')
     generator = FactGenerator(handler.merged_lines, '../facts/facts.txt')
     generator.generate_fact()
-    print(not generator.facts)
-    print('-------lines-------')
+    print('-------lines-------' + str(len(handler.merged_lines)))
     for line in handler.merged_lines:
         print(line)
     print('-------end---------')
-    print('-------facts-------')
+    print('-------facts-------' + str(len(generator.facts)))
     for fact in generator.facts:
         handler.draw_lines(fact.about)
         print(fact)
     print('-------end---------')
+    show_image(handler.hide, 'hough')
     show_image(handler.rst, 'result')
+    show_image(handler.dst, 'canny')
     WaitKey(0)
